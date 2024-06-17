474a4975c636342bd25b4c189e32ef41a3f33131
Sid Sabhnani <ssabhnani@utexas.edu>
2024-06-17 16:21:38 -0700

    cook

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,141 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
-use serde::Deserialize;
+mod util;
 
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use util::*;
+
+const ADDRESS: &str = "localhost:3000";
+
+// TODO: add a terminal ui to start the server so the TA can customize their office hours details etc and post to the HTTP
+
+/// This function starts the server and defines the routes for the web application.
 #[actix_web::main]
 async fn main() {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(get_index))
-            .route("/gcd", web::post().to(post_gcd))
+            .route(
+                "/",
+                web::get().to(|| async {
+                    let html = std::fs::read_to_string("src/public/index.html")
+                        .expect("error reading index.html");
+                    HttpResponse::Ok().content_type("text/html").body(html)
+                }),
+            )
+            .route(
+                "/waiting",
+                web::get().to(|| async {
+                    let html = std::fs::read_to_string("src/public/waiting.html")
+                        .expect("error reading waiting.html");
+                    HttpResponse::Ok().content_type("text/html").body(html)
+                }),
+            )
+            .route(
+                "/done",
+                web::get().to(|| async {
+                    let html = std::fs::read_to_string("src/public/done.html")
+                        .expect("error reading done.html");
+                    HttpResponse::Ok().content_type("text/html").body(html)
+                }),
+            )
+            .service(fs::Files::new("/static", "src/public").show_files_listing())
+            .route("/api/join", web::post().to(join_queue))
+            .route("/api/leave", web::post().to(leave_queue))
+            .route("/api/position", web::get().to(get_position)) // Changed to GET to match typical RESTful semantics
     });
 
-    println!("Serving on http://localhost:3000...");
+    println!("Serving on {}...", ADDRESS);
     server
-        .bind("localhost:3000").expect("error binding server to address")
+        .bind(ADDRESS)
+        .expect("error binding server to address")
         .run()
         .await
         .expect("error running server");
 }
-// this interaction between the HTML and the endpoint is a violation of the separation of concerns principle
-// why? because the HTML form is a String literal in the Rust code while the endpoint is a function that returns an HttpResponse
-async fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(
-            r#"
-                <title>GCD Calculator</title>
-                <form action="/gcd" method="post">
-                <input type="text" name="n"/>
-                <input type="text" name="m"/>
-                <button type="submit">Compute GCD</button>
-                </form>
-            "#,
-        )
-}
 
-#[derive(Deserialize)]
-struct GcdParameters {
-    n: u64,
-    m: u64,
-}
-
-async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
-    if form.n == 0 || form.m == 0 {
+/// This function is called when a student submits a request to join the office hour queue.
+/// It takes a form as input and returns an HTTP response.
+async fn join_queue(form: web::Form<StudentInfo>) -> HttpResponse {
+    if form.name.is_empty()
+        || form.csid.is_empty()
+        || form.purpose.is_empty()
+        || form.details.is_empty()
+        || form.steps.is_empty()
+    {
         return HttpResponse::BadRequest()
             .content_type("text/html")
-            .body("Computing the GCD with zero is boring.");
+            .body("Please complete the requirements to join the office hour queue.");
     }
 
-    let response =
-        format!("The greatest common divisor of the numbers {} and {} \
-                 is <b>{}</b>\n",
-                form.n, form.m, gcd(form.n, form.m));
+    let student_info = StudentInfo::new(
+        form.name.clone(),
+        form.csid.clone(),
+        form.purpose.clone(),
+        form.details.clone(),
+        form.steps.clone(),
+    );
 
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(response)
+    let student_request = StudentRequest::new(student_info);
+
+    match handle_join(student_request.clone()) {
+        Ok(_) => {
+            // send to /waiting?id
+            HttpResponse::Found()
+                .append_header(("Location", format!("/waiting?id={}", student_request.id)))
+                .finish()
+        }
+        Err(_) => HttpResponse::InternalServerError()
+            .content_type("text/html")
+            .body("An error occurred while processing your request."),
+    }
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
+/// This function handles the student request by appending to the queue.
+fn handle_join(student_request: StudentRequest) -> Result<(), ()> {
+    // to do:
+    // figure out how to have a file-based queue
+    // append to the queue
+    // return Ok(()) if successful
+    // return Err(()) if unsuccessful
+    println!("Request created {:?}", student_request);
+    Ok(())
+}
+
+async fn leave_queue(query: web::Query<IdQuery>) -> HttpResponse {
+    match handle_leave(query.id.clone()) {
+        Ok(_) => HttpResponse::Found().append_header(("Location", "/done")).finish(),
+        Err(_) => HttpResponse::InternalServerError()
+            .content_type("text/html")
+            .body("An error occurred while processing your request."),
     }
-    return n
+}
+
+
+fn handle_leave(id: String) -> Result<(), ()> {
+    // to do:
+    // figure out how to have a file-based queue
+    // remove from the queue
+    // return Ok(()) if successful
+    // return Err(()) if unsuccessful
+    println!("Request removed for ID: {}", id);
+    Ok(())
+}
+
+async fn get_position(query: web::Query<IdQuery>) -> HttpResponse {
+    println!("Position requested for ID: {}", query.id);
+    match handle_position(query.id.clone()) {
+        Ok(position) => HttpResponse::Ok().content_type("text/html").body(position),
+        Err(_) => HttpResponse::InternalServerError()
+            .content_type("text/html")
+            .body("An error occurred while processing your request."),
+    }
+}
+
+fn handle_position(id: String) -> Result<String, ()> {
+    // to do:
+    // figure out how to have a file-based queue
+    // get the position of the student in the queue
+    // return Ok(position) if successful
+    // return Err(()) if unsuccessful
+    println!("Position requested for ID: {}", id);
+    Ok("1".to_string())
 }
---
+++ src/util.rs
@@ -0,0 +1,64 @@
+use serde::Deserialize;
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize, Clone)]
+pub enum Purpose {
+    ConceptualMaterial,
+    ConceptualLab,
+    Debugging,
+    Other,
+}
+
+impl Purpose {
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Purpose::ConceptualMaterial => false,
+            Purpose::ConceptualLab => false,
+            Purpose::Debugging => false,
+            Purpose::Other => false,
+        }
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct StudentInfo {
+    pub name: String,
+    pub csid: String,
+    pub purpose: Purpose,
+    pub details: String,
+    pub steps: String,
+}
+
+impl StudentInfo {
+    pub fn new(name: String, csid: String, purpose: Purpose, details: String, steps: String) -> StudentInfo {
+        StudentInfo {
+            name,
+            csid,
+            purpose,
+            details,
+            steps,
+        }
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct StudentRequest {
+    pub info : StudentInfo,
+    pub id: String,
+}
+
+impl StudentRequest {
+    pub fn new (info: StudentInfo) -> StudentRequest {
+        StudentRequest {
+            info,
+            id: Uuid::new_v4().to_string(),
+        }
+    }
+}
+
+// Supporting struct for query parameters
+#[derive(Deserialize)]
+pub struct IdQuery {
+    pub id: String,
+}
+
